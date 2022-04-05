f89f445766b611c0204ee6474497157692254cb2
federicoalferreri <federicoangelo.ferreri@mail.polimi.it>
2022-04-05 17:09:47 +0200

    add SchoolBoard

--- src/main/java/it/polimi/ingsw/Model/SchoolBoard.java
+++ src/main/java/it/polimi/ingsw/Model/SchoolBoard.java
@@ -7,7 +7,7 @@ public class SchoolBoard {
     private List<Color> Entrance= new ArrayList<Color>();
     /*private DiningRoom= new DiningRoom();*/
     private List<Color> ProfessorTable= new ArrayList<Color>();
-    /*private <ColorTower> TowerRoom= new ArrayList<ColorTower>();*/
+    private List<ColorTower> TowerRoom= new ArrayList<ColorTower>();
 
     public SchoolBoard(){
         NumberId=0;
@@ -16,23 +16,32 @@ public class SchoolBoard {
     public void GetId(int id){      /* giving an id to the school_board*/
         NumberId=id;
     }
-    public void PutProfessor(Color p){    /* putting a professor in the professor table*/
+    public void PutProfessor(Color p){    /* putting professor p in the professor table*/
         ProfessorTable.add(p);
     }
-    /*public boolean ExistProfessor(Color p){         saying if there is the professor p on the professor table or not
-        if(ProfessorTable.get(Color.GetIndex)==p){
-                return true;
-        }
-        else{
+    public boolean ExistProfessor(Color p) {         /*saying if there is the professor p on the professor table or not*/
+        if (ProfessorTable.get(p.getIndex()) != null) {
+            return true;
+        } else {
             return false;
-        }*/
+        }
+    }
     public void RemoveProfessor(Color p){      /*removing professor p from the professor table*/
         ProfessorTable.remove(p);
     }
-    /*public int GetNumberTower(){     Getting the number of the towers in the tower room
+    public int GetNumberTower(){     /*getting number of towers in the tower room*/
         return TowerRoom.size() ;
-    }*/
-    /*public void PutTower(ColorTower t){      putting tower t in the tower room
+    }
+    public void PutTower(ColorTower t){      /*putting tower t in the tower room*/
         TowerRoom.add(t);
-    }*/
+    }
+    public void PutStudent(Color s){     /*putting student s in the entrance*/
+        Entrance.add(s);
+    }
+    public void RemoveStudent(Color s){     /*removing student s from the entrance*/
+        Entrance.remove(s);
+    }
+    public int GetNumberStudent(){       /*getting number of students in the entrance*/
+        return Entrance.size();
+    }
 }
