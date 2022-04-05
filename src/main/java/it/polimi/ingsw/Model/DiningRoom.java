22be0e3a8b238495bb36347585a7e6b6eb51e5e8
federicoalferreri <federicoangelo.ferreri@mail.polimi.it>
2022-04-05 17:47:07 +0200

    add DiningRoom

---
+++ src/main/java/it/polimi/ingsw/Model/DiningRoom.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.Model;
+import java.util.*;
+import java.util.ArrayList;
+
+public class DiningRoom {
+    private ArrayList<ArrayList<Color>> DiningRoom=new ArrayList<ArrayList<Color>>();
+
+    /*public void PutStudent(Color s){     putting student s in the dining room
+        DiningRoom.add(s);
+    }
+    public void RemoveStudent(Color s) {     removing student s from the dining room
+        DiningRoom.remove(s);
+    }
+    public int GetNumberStudent(){       getting number of students in the dining room
+        return DiningRoom.size();
+    }
+    public boolean GetCoin(){
+
+    }*/
+
+}
