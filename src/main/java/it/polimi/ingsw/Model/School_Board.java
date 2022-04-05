eccd02ff7521532cf8e31e1a3e9993ee5abeb1c0
federicoalferreri <federicoangelo.ferreri@mail.polimi.it>
2022-04-05 15:41:23 +0200

    add School_Board

---
+++ src/main/java/it/polimi/ingsw/Model/School_Board.java
@@ -0,0 +1,14 @@
+/*package it.polimi.ingsw.Model;
+import java.util.*;
+
+public class School_Board {
+    private int NumberId;
+    private int NumberTower;
+    private int Student;
+    private List<Color> Entrance= new ArrayList<Color>();
+    private Dining_Room= new Dining_Room();
+    private List<Color> Professor_Table= new ArrayList<Color>();
+    private Color_Tower[] Tower_Room= new Color_Tower[8];
+
+
+}*/
