757af9105b7ac792a6510a87d880f0490bc6bcfe
federicoalferreri <federicoangelo.ferreri@mail.polimi.it>
2022-04-08 11:38:34 +0200

    add CharacterCard

---
+++ src/main/java/it/polimi/ingsw/Model/CharacterCard.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.Model;
+
+public class CharacterCard {
+    private int Name;
+    private int Coins;
+    private int CountUse;
+
+    public CharacterCard(int name, int coins, int countUse) {
+        Name= name;
+        Coins= coins;
+        CountUse= countUse;
+    }
+
+    public int getName(){      /* return the character's name*/
+        return Name;
+    }
+    public int getCountUse() {    /*how much the card is used*/
+        return CountUse;
+    }
+    public int getCost(){       /*card's cost*/
+        return Coins+CountUse;
+    }
+}
