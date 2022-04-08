5990c75d7142337d538883411b1a44cd4a1cfe9b
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-04-08 11:56:07 +0200

    fixed some CloudTiles methods and added Bag class

---
+++ src/main/java/it/polimi/ingsw/Model/Bag.java
@@ -0,0 +1,35 @@
+package it.polimi.ingsw.Model;
+import java.util.*;
+
+/**
+ * @author Lorenzo Forgia
+ */
+public class Bag {
+    private List<Color> students=new ArrayList<Color>();
+
+/** setStudents: in the beginning of game 10 students,then 120 **/
+    public void setStudents(int numstudents) {
+        Color[] c=Color.values();
+        for(Color colorstudent:c){
+            for(int i=0;i<(numstudents/5);i++){
+                students.add(colorstudent);
+            }
+        }
+    }
+/** method to draft students randomly**/
+    public Color CatchStudent() {
+        Random random = new Random();
+        int draftedindex = random.nextInt(this.students.size());
+        Color draftedcolor = this.students.get(draftedindex);
+        this.students.remove(draftedindex);
+        return draftedcolor;
+    }
+
+    /** method to insert student in bag ,for example when each player must remove 3 students from his
+     * dining room
+     */
+    public void PutStudent(Color stud){
+        this.students.add(stud);
+    }
+
+}
