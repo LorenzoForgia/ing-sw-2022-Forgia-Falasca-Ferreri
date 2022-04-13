03109f694b30627236abc72c1f77786e45ae33c6
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-04-13 18:12:20 +0200

    added CloudTilesTest

--- src/test/java/it/polimi/ingsw/Model/CharacterCard9Test.java
+++ src/test/java/it/polimi/ingsw/Model/CharacterCard9Test.java
@@ -1,7 +1,29 @@
 package it.polimi.ingsw.Model;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterCard9Test {
+    @Test
+    public void testColorNoEffect1(){
+        IslandTiles i=new IslandTiles(1,false,1,false,false);
+        i.PutStudent(Color.Red);
+        i.PutStudent(Color.Blue);
+        i.PutStudent(Color.Yellow);
+        i.PutStudent(Color.Red);
+        i.PutStudent(Color.Red);
+        CharacterCard9 c=new CharacterCard9(9,1,2);
+        assertEquals(2,c.ColorNoEffect(i,Color.Red,5));
+    }
+    @Test
+    public void testColorNoEffect2(){
+        IslandTiles i=new IslandTiles(1,false,1,true,false);
+        i.PutStudent(Color.Red);
+        i.PutStudent(Color.Blue);
+        i.PutStudent(Color.Yellow);
+        i.PutStudent(Color.Red);
+        i.PutStudent(Color.Red);
+        CharacterCard9 c=new CharacterCard9(9,1,2);
+        assertEquals(0,c.ColorNoEffect(i,Color.Red,0));
+    }
 
 }
