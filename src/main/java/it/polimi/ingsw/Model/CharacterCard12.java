ec6a0a7a15a987f2594b791f0ad1a7a440e1de24
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-04-14 18:01:50 +0200

    added Charactercar12Test

--- src/main/java/it/polimi/ingsw/Model/CharacterCard12.java
+++ src/main/java/it/polimi/ingsw/Model/CharacterCard12.java
@@ -4,13 +4,14 @@ public class CharacterCard12 extends CharacterCard{
     public CharacterCard12(int name, int coins, int countUse) {
         super(name, coins, countUse);
     }
-    public void useEffect12(Color c,Player[] players){
+    public void useEffect12(Color c,Player[] players,Bag b){
         for(int i=0,limit=3;i<players.length;i++,limit=3){
             if(players[i].getMySchoolBoard().getDiningRoom().GetNumberStudent(c)<3){
                 limit=players[i].getMySchoolBoard().getDiningRoom().GetNumberStudent(c);
             }
             for(int j=0; j<limit;j++){
                 players[i].getMySchoolBoard().getDiningRoom().RemoveStudent(c);
+                b.PutStudent(c);
             }
         }
     }
--- src/test/java/it/polimi/ingsw/Model/CharacterCard12Test.java
+++ src/test/java/it/polimi/ingsw/Model/CharacterCard12Test.java
@@ -1,7 +1,53 @@
 package it.polimi.ingsw.Model;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CharacterCard12Test {
+   @Test
+    public void testUseEffect12(){
+       Bag b=new Bag();
+       SchoolBoard s1=new SchoolBoard(1,1);
+       SchoolBoard s2=new SchoolBoard(2,2);
+       SchoolBoard s3=new SchoolBoard(3,3);
+       SchoolBoard s4=new SchoolBoard(4,4);
+       DeckCardAssistant d1=new DeckCardAssistant();
+       DeckCardAssistant d2=new DeckCardAssistant();
+       DeckCardAssistant d3=new DeckCardAssistant();
+       DeckCardAssistant d4=new DeckCardAssistant();
+       Player p1=new Player("A",10,s1,d1);
+       Player p2=new Player("B",10,s2,d2);
+       Player p3=new Player("C",10,s3,d3);
+       Player p4=new Player("D",10,s4,d4);
+       s1.getDiningRoom().PutStudent(Color.Red);       /** Player1 has 1 Red students **/
+       s1.getDiningRoom().PutStudent(Color.Green);
+       s2.getDiningRoom().PutStudent(Color.Red);       /** Player2 has 2 Red students **/
+       s2.getDiningRoom().PutStudent(Color.Red);
+       s2.getDiningRoom().PutStudent(Color.Blue);
+       s3.getDiningRoom().PutStudent(Color.Red);     /** Player3 has 3 Red students **/
+       s3.getDiningRoom().PutStudent(Color.Red);
+       s3.getDiningRoom().PutStudent(Color.Red);
+       s3.getDiningRoom().PutStudent(Color.Pink);
+       s4.getDiningRoom().PutStudent(Color.Blue);    /** Player4 has 0 red students**/
+       s4.getDiningRoom().PutStudent(Color.Green);
+       s4.getDiningRoom().PutStudent(Color.Yellow);
+       CharacterCard12 c=new CharacterCard12(12,8,2);
+       Player[] ArrPlay=new Player[4];
+       ArrPlay[0]=p1;
+       ArrPlay[1]=p2;
+       ArrPlay[2]=p3;
+       ArrPlay[3]=p4;
+       assertTrue(b.BagIsEmpty());
+       c.useEffect12(Color.Red,ArrPlay,b);
+       assertEquals(6,b.getStudents().size());
+       assertEquals(0,s1.getDiningRoom().GetNumberStudent(Color.Red));
+       assertEquals(1,s1.getDiningRoom().GetNumberStudent());
+       assertEquals(0,s2.getDiningRoom().GetNumberStudent(Color.Red));
+       assertEquals(1,s2.getDiningRoom().GetNumberStudent());
+       assertEquals(0,s3.getDiningRoom().GetNumberStudent(Color.Red));
+       assertEquals(1,s3.getDiningRoom().GetNumberStudent());
+       assertEquals(0,s4.getDiningRoom().GetNumberStudent(Color.Red));
+       assertEquals(3,s4.getDiningRoom().GetNumberStudent());
+
+   }
 
 }
