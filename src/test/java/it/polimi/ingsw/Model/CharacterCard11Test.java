2cb29dadd6a6517c1e4abfd9361e397a45bb9eb3
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-04-14 16:01:27 +0200

    added Charactercar11

--- src/test/java/it/polimi/ingsw/Model/CharacterCard11Test.java
+++ src/test/java/it/polimi/ingsw/Model/CharacterCard11Test.java
@@ -16,7 +16,24 @@ class CharacterCard11Test {
        assertEquals(true,b.BagIsEmpty());
        assertEquals(4,c.getStudents().size());
    }
-/*
     @Test
-    */
+    public void testuseEffetc11(){
+        Bag b=new Bag();
+        b.PutStudent(Color.Green);
+        b.PutStudent(Color.Yellow);
+        b.PutStudent(Color.Blue);
+        b.PutStudent(Color.Pink);
+        CharacterCard11 c=new CharacterCard11(11,1,1);
+        c.draft4students(b);
+        b.PutStudent(Color.Red);
+        assertFalse(b.BagIsEmpty());
+        assertEquals(4,c.getStudents().size());
+        SchoolBoard sb= new SchoolBoard(1,1);
+        sb.getDiningRoom().PutStudent(Color.Red);
+        sb.getDiningRoom().PutStudent(Color.Blue);
+        sb.getDiningRoom().PutStudent(Color.Red);
+        sb.getDiningRoom().PutStudent(Color.Red);
+        c.useEffect11(Color.Green,sb,b);
+        assertEquals(4,c.getStudents().size());
+    }
 }
