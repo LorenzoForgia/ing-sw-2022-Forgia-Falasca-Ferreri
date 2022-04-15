12416656cd43a676111f23b1c529a60e993c7df7
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-04-15 12:58:12 +0200

    added Controller setup

--- src/main/java/it/polimi/ingsw/Controller/Setup.java
+++ src/main/java/it/polimi/ingsw/Controller/Setup.java
@@ -1,4 +1,82 @@
 package it.polimi.ingsw.Controller;
+import it.polimi.ingsw.Model.*;
 
+import java.util.*;
 public class Setup {
+
+   /** forse non serve
+    public List<ColorTower> getTowers(ColorTower ct){
+        List<ColorTower> towers=new ArrayList<>();
+        int limit=8;
+        if(ct.equals(ColorTower.Grey)){
+           limit=6;
+        }
+        for(int i=0;i<limit;i++){
+            towers.add(ct);
+        }
+        return towers;
+    }**/
+
+
+    public Bag CreateBag(){
+        Bag b=new Bag();
+        return b;
+    }
+    public void SetBag(Bag b,int numsetbag){    /** setStudents in 2 phases**/
+        if(numsetbag==1) {
+            b.setStudents(10);
+        }else{
+            b.setStudents(120);
+        }
+    }
+
+    public List<IslandTiles> CreateIslandTiles(int MNposition){
+        List<IslandTiles> it =new ArrayList<IslandTiles>();
+        Boolean MotherNature=false;
+        for(int i=0;i<12;i++){
+            if(i==MNposition){
+                MotherNature=true;
+            }
+              it.add(new IslandTiles(i,MotherNature,1,false,false));
+              MotherNature=false;
+        }
+        return it;
+    }
+    public void SetupStudentsInIslands(Bag b,int MNposition,List<IslandTiles> is){
+        int starter=MNposition;
+        int infrontofMN=(MNposition+6)%12;
+        if(starter==11){
+            starter=0;
+        }
+        for(int i=0;i<12;i++){
+            if(i!=MNposition && i!=infrontofMN){
+                is.get(i).PutStudent(b.CatchStudent());
+            }
+        }
+    }
+
+
+     public GeneralBoard getGeneralboard(ChooseSettingGame sg){
+         if(sg.isModExpert()){
+             return new GeneralBoard(20);
+         }else{
+             return new GeneralBoard(0);
+         }
+     }
+
+
+
+
+   /**public List<ColorTower> setupTowers(ChooseSettingGame sg){
+
+   }**/
+ /*  public void SetGame(ChooseSettingGame settingsgame, List<SchoolBoard> sb, List<ColorTower> ColorTow){
+      if(settingsgame.getNumPlayers()==2){
+          for(int i=0;i<2;i++){
+              ColorTower ct=ColorTow.get(i);
+              sb.get(i).setColorTower(ct);
+
+          }
+      }
+   }*/
 }
