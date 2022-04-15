b1ff860adb76cf05ab0a239967f32c2f1d812eb3
federicoalferreri <federicoangelo.ferreri@mail.polimi.it>
2022-04-15 15:40:16 +0200

    add GameEndState

--- src/main/java/it/polimi/ingsw/Controller/ChoosenPlayer.java
+++ src/main/java/it/polimi/ingsw/Controller/ChoosenPlayer.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class ChoosenPlayer {
     public void ChooseTurnPlayer(ArrayList<CardAssistant> c, Player p){
         for(int i=0;i<c.size();i++){
-            for(int j=1;j<c.size();j++){
+            for(int j=i+1;j<c.size();j++){
                 if(c.get(i).getCardValue()>c.get(j).getCardValue()){
                     if(p.getMySchoolBoard().GetId()==i){
                         p.SetNumTurn();
---
+++ src/main/java/it/polimi/ingsw/Controller/GameEndState.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.Controller;
+
+import it.polimi.ingsw.Model.Bag;
+import it.polimi.ingsw.Model.GeneralBoard;
+import it.polimi.ingsw.Model.Player;
+
+public class GameEndState {
+    public boolean CheckEndGameImmediately(Player p, GeneralBoard g) {
+        if (p.getMySchoolBoard().GetNumberTower() == 0){
+            return true;
+        } else if(g.GetIslands().size() == 3){
+            return true;
+        }else{return false;}
+    }
+    public boolean CheckEndGameRoundEnded(Player p, GeneralBoard g, Bag b){
+        if(b.BagIsEmpty()){
+            return true;
+        }else if(p.getMyDeck().GetDeck().size()==0){
+            return true;
+        }else{return false;}
+    }
+}
