922c4f322e95a6a04824d2389f900fd8ec1e8f20
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-04-22 11:30:13 +0200

    added ChooseCloudTiles and ChooseCloudTilesTest

--- src/main/java/it/polimi/ingsw/Controller/ChooseCloudTiles.java
+++ src/main/java/it/polimi/ingsw/Controller/ChooseCloudTiles.java
@@ -5,11 +5,10 @@ import it.polimi.ingsw.Model.Player;
 
 public class ChooseCloudTiles {
     public void ChoosenCloud(Player p, CloudTiles c){
-        for(int i=0; i<c.getStud().size();i++){
-            p.getMySchoolBoard().getEntrance().add(c.getStud().get(i));
-        }
-        for(int i=0; i<c.getStud().size();i++){
-           c.getStud().remove(i);
+        int limit=c.getStud().size();
+        for(int i=0; i<limit;i++){
+            p.getMySchoolBoard().getEntrance().add(c.getStud().get(0));
+            c.getStud().remove(0);
         }
     }
 }
