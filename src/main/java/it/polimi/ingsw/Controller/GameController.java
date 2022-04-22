35be1a5807ef694f79c47989f0df5ac1e9a67077
LuigiaFalasca <luigia.falasca@mail.polimi.it>
2022-04-22 14:47:17 +0200

    Added GameController

---
+++ src/main/java/it/polimi/ingsw/Controller/GameController.java
@@ -0,0 +1,42 @@
+package it.polimi.ingsw.Controller;
+
+import it.polimi.ingsw.Model.Player;
+
+import java.util.List;
+
+public class GameController {
+    private AddStudentsOnClouds addStudentsOnClouds = new AddStudentsOnClouds();
+    private ChooseCloudTiles chooseCloudTiles = new ChooseCloudTiles();
+    private ChoosenPlayer choosenPlayer = new ChoosenPlayer();
+    private ChooseSettingGame chooseSettingGame;
+    private GameEndState gameEndState = new GameEndState();
+    private Move3Students move3Students = new Move3Students();
+    private Setup setup = new Setup();
+    private MoveMotherNature moveMotherNature = new MoveMotherNature();
+    private WinLoseCheckState winLoseCheckState = new WinLoseCheckState();
+    private PlayAssCard playAssCard = new PlayAssCard();
+    private List<Player> players;
+
+    public static void main(String[] args) {
+        /* Richiesta modalità gioco
+        chooseSettingGame= new ChooseSettingGame();
+        Metodi di SetUp
+        Creazione giocatori con i rispettivi nickname
+        Inizia un ciclo con un flag dato dal Game End State
+        AddStudentOnCloud
+        Scegli un giocatore random che sceglierà la carta assistente
+        prendi tutte le carte assistenti dagli altri giocatori
+        chiama play ass card
+        inizia il turno del primo giocatore tramite un ciclo for (choosenPlayer)
+        Scelta location da parte del player
+        Move3student
+        MoveMotherNature
+        CheckEndGameState
+        se true uscire dal loop, altrimenti ricominciare
+        se usciti dal loop winlosecheckstate
+
+        */
+    }
+}
+
+
