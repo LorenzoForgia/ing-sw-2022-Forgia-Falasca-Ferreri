1a36bbce7a6f5e9a726b6dddb463aea44ed2bd43
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-05-09 16:01:01 +0200

    add server/client

---
+++ src/main/java/it/polimi/ingsw/Client/views/GameCreatedView.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.Client.views;
+
+import it.polimi.ingsw.messages.AnsGameCreatedMsg;
+import it.polimi.ingsw.messages.BooleanCheckMsg;
+import it.polimi.ingsw.messages.NewGameMsg;
+
+import java.util.Scanner;
+
+public class GameCreatedView extends View{
+    /** The view of the gamecreated */
+
+    private BooleanCheckMsg answerMsg;
+
+
+    public GameCreatedView(BooleanCheckMsg answerMsg)
+    {
+        this.answerMsg = answerMsg;
+    }
+
+
+    @Override
+    public void run() {
+
+        System.out.println("Game created");
+    }
+}
