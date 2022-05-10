a4b8c4f96161a83ffb470c8d0a178426430d0747
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-05-10 15:55:04 +0200

    add server/client

---
+++ src/main/java/it/polimi/ingsw/Client/views/GameStartedView.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.Client.views;
+
+import it.polimi.ingsw.messages.AnsGameCreatedMsg;
+import it.polimi.ingsw.messages.BooleanCheckMsg;
+import it.polimi.ingsw.messages.NewGameMsg;
+
+import java.util.Scanner;
+
+public class GameStartedView extends View{
+    /** The view of the gamestarted */
+
+  /*  private AnsGameStartedMsg answerMsg;*/
+
+
+   /* public GameCreatedView(AnsGameStartedMsg answerMsg)
+    {
+        this.answerMsg = answerMsg;
+    }*/
+
+
+    @Override
+    public void run() {
+
+        System.out.println("Il gioco inizia:");
+
+
+    }
+}
