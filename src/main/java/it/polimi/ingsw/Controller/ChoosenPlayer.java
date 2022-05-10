6021e68d5faec3e4af1e0ae4f9cb60e56f6228b9
LuigiaFalasca <luigia.falasca@mail.polimi.it>
2022-05-10 18:03:17 +0200

    Added methods in ChoosenPlayer

--- src/main/java/it/polimi/ingsw/Controller/ChoosenPlayer.java
+++ src/main/java/it/polimi/ingsw/Controller/ChoosenPlayer.java
@@ -4,8 +4,10 @@ import it.polimi.ingsw.Model.CardAssistant;
 import it.polimi.ingsw.Model.Player;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ChoosenPlayer {
+    private List<Player> orderPlayers;
     public void ChooseTurnPlayer(ArrayList<CardAssistant> c, Player p){
         for(int i=0;i<c.size();i++){
             for(int j=0;j<c.size();j++){
@@ -15,6 +17,29 @@ public class ChoosenPlayer {
                     }
                 }
             }
+        }
+    }
+
+    public List<Player> GetOrderPlayers(List<Player> players){
+        orderPlayers = new ArrayList<Player>();
+        for(int i =0; i < players.size(); i ++ ){
+            orderPlayers.add(players.get(i).GetNumTurn(), players.get(i));
+        }
+        return orderPlayers;
+    }
+
+    public Player GetPlayerTurn(){
+        Player p;
+        p= orderPlayers.get(0);
+        orderPlayers.remove(0);
+        return p;
+    }
+
+    public boolean EndOfAllTurn(){
+        if(orderPlayers.size()== 0){
+            return true;
+        }else{
+            return false;
         }
     }
 }
