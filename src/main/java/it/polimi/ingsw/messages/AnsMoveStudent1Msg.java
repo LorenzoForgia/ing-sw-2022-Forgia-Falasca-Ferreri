90bea61920fffb43a62fce85fd87f993aeaac040
federicoalferreri <federicoangelo.ferreri@mail.polimi.it>
2022-05-17 17:13:25 +0200

    View CLI

--- src/main/java/it/polimi/ingsw/messages/AnsMoveStudent1Msg.java
+++ src/main/java/it/polimi/ingsw/messages/AnsMoveStudent1Msg.java
@@ -2,14 +2,22 @@ package it.polimi.ingsw.messages;
 
 import it.polimi.ingsw.Client.ServerHandler;
 import it.polimi.ingsw.Client.views.MoveStudent2View;
+import it.polimi.ingsw.Model.GeneralBoard;
+import it.polimi.ingsw.Model.Player;
+
+import java.util.ArrayList;
 
 public class AnsMoveStudent1Msg extends AnswerMsg{
     private String name;
+    private GeneralBoard generalBoard;
+    private ArrayList<Player> players= new ArrayList<Player>();
 
 
-    public AnsMoveStudent1Msg(CommandMsg parent, String name){
+    public AnsMoveStudent1Msg(CommandMsg parent, String name,GeneralBoard generalBoard,ArrayList<Player> players ){
         super(parent);
         this.name = name;
+        this.generalBoard= generalBoard;
+        this.players=players;
     }
     @Override
     public void processMessage(ServerHandler serverHandler)
@@ -19,5 +27,7 @@ public class AnsMoveStudent1Msg extends AnswerMsg{
     public String GetPlayer(){
         return name;
     }
+    public GeneralBoard GetGB(){return generalBoard;}
+    public ArrayList<Player> GetPlayers(){return players;}
 }
 
--- src/main/java/it/polimi/ingsw/messages/AnsMoveStudent2Msg.java
+++ src/main/java/it/polimi/ingsw/messages/AnsMoveStudent2Msg.java
@@ -3,13 +3,21 @@ package it.polimi.ingsw.messages;
 import it.polimi.ingsw.Client.ServerHandler;
 import it.polimi.ingsw.Client.views.MoveStudent2View;
 import it.polimi.ingsw.Client.views.MoveStudent3View;
+import it.polimi.ingsw.Model.GeneralBoard;
+import it.polimi.ingsw.Model.Player;
+
+import java.util.ArrayList;
 
 public class AnsMoveStudent2Msg extends AnswerMsg{
     private String name;
+    private GeneralBoard generalBoard;
+    private ArrayList<Player> players= new ArrayList<Player>();
 
-    public AnsMoveStudent2Msg(CommandMsg parent, String name){
+    public AnsMoveStudent2Msg(CommandMsg parent, String name, GeneralBoard generalBoard,ArrayList<Player> players){
         super(parent);
         this.name = name;
+        this.generalBoard= generalBoard;
+        this.players=players;
     }
     @Override
     public void processMessage(ServerHandler serverHandler)
@@ -19,4 +27,6 @@ public class AnsMoveStudent2Msg extends AnswerMsg{
     public String GetPlayer(){
         return name;
     }
+    public GeneralBoard GetGB(){return generalBoard;}
+    public ArrayList<Player> GetPlayers(){return players;}
 }
