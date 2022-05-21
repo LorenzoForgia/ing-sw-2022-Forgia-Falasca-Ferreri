e883124d1ca36979fd3f8b0f54373daa5ecefd81
LuigiaFalasca <luigia.falasca@mail.polimi.it>
2022-05-21 10:14:46 +0200

    Fixed bugs and tests in Controller

--- src/main/java/it/polimi/ingsw/Controller/WinLoseCheckState.java
+++ src/main/java/it/polimi/ingsw/Controller/WinLoseCheckState.java
@@ -5,118 +5,79 @@ import it.polimi.ingsw.Model.*;
 import java.util.*;
 
 public class WinLoseCheckState {
-    public boolean CheckIfWinWithTower(Player p, GeneralBoard g){
-        int w=0,b=0,gr=0;
-        for(int i=0;i<g.GetIslands().size();i++){
-            if( g.GetIslands().get(i).isTower() && g.GetIslands().get(i).getColTower().equals(ColorTower.Black)){
-                b=b+g.GetIslands().get(i).getSize();
-            }else if(g.GetIslands().get(i).isTower() && g.GetIslands().get(i).getColTower().equals(ColorTower.White)){
-                w=w+g.GetIslands().get(i).getSize();
-            }else if(g.GetIslands().get(i).isTower() && g.GetIslands().get(i).getColTower().equals(ColorTower.Grey)){
-                gr=gr+g.GetIslands().get(i).getSize();
-            }
-        }
-        if(b>w && b>gr && p.getMySchoolBoard().ColorTower().equals(ColorTower.Black)){
-            return true;
-        }else if(w>b && w>gr && p.getMySchoolBoard().ColorTower().equals(ColorTower.White)){
-            return true;
-        }else if(gr>w && gr>b && p.getMySchoolBoard().ColorTower().equals(ColorTower.Grey)){
-            return true;
-        }else {return false;}
-    }
 
-    public ArrayList<Boolean> CheckWinner(ArrayList<Player> players, List<SchoolBoard> SbWithSchoolBoard){
-        int min=9;
-        ArrayList<ColorTower> colorWinning= new ArrayList<>();
-        ArrayList<Boolean> playersVictory= new ArrayList<>();
+    public ArrayList<Boolean> CheckWinner(ArrayList<Player> players, List<SchoolBoard> SbWithSchoolBoard) {
+        int min = 9;
+        ArrayList<ColorTower> colorWinning = new ArrayList<>();
+        ArrayList<Boolean> playersVictory = new ArrayList<>();
 
-        for(int i=0; i< players.size(); i++){
+        for (int i = 0; i < players.size(); i++) {
             playersVictory.add(false);
         }
 
-        for(int i=0; i< SbWithSchoolBoard.size(); i++){
-            if(SbWithSchoolBoard.get(i).getNumberOfTower()< min){
-                colorWinning= new ArrayList<>();
+        for (int i = 0; i < SbWithSchoolBoard.size(); i++) {
+            if (SbWithSchoolBoard.get(i).getNumberOfTower() < min) {
+                colorWinning = new ArrayList<>();
                 colorWinning.add(SbWithSchoolBoard.get(i).ColorTower());
                 min = SbWithSchoolBoard.get(i).getNumberOfTower();
-            }else if(SbWithSchoolBoard.get(i).getNumberOfTower()== min){
+            } else if (SbWithSchoolBoard.get(i).getNumberOfTower() == min) {
                 colorWinning.add(SbWithSchoolBoard.get(i).ColorTower());
             }
         }
 
-        if(colorWinning.size()>1){
-            colorWinning= CheckProfessor(colorWinning, players);
+        if (colorWinning.size() > 1) {
+            colorWinning = CheckProfessor(colorWinning, players);
         }
 
-        for(int i=0; i< players.size(); i++ ){
-            for(int j=0; j < colorWinning.size(); j++){
-                if(players.get(i).getMySchoolBoard().ColorTower().equals(colorWinning.get(j))){
-                    playersVictory.set(i,true);
+        for (int i = 0; i < players.size(); i++) {
+            for (int j = 0; j < colorWinning.size(); j++) {
+                if (players.get(i).getMySchoolBoard().ColorTower().equals(colorWinning.get(j))) {
+                    playersVictory.set(i, true);
                 }
             }
         }
         return playersVictory;
     }
 
-    private ArrayList<ColorTower> CheckProfessor(ArrayList<ColorTower> colorWinning, ArrayList<Player> players){
-        int max=0;
-        ArrayList<Color> ProfessorTableBlack= new ArrayList<>();
-        ArrayList<Color> ProfessorTableWhite= new ArrayList<>();
-        ArrayList<ColorTower> colorTowers= new ArrayList<>();
-        if(players.size()==3) {
+    private ArrayList<ColorTower> CheckProfessor(ArrayList<ColorTower> colorWinning, ArrayList<Player> players) {
+        int max = 0;
+        ArrayList<Color> ProfessorTableBlack = new ArrayList<>();
+        ArrayList<Color> ProfessorTableWhite = new ArrayList<>();
+        ArrayList<ColorTower> colorTowers = new ArrayList<>();
+        if (players.size() == 3) {
             for (int i = 0; i < colorWinning.size(); i++) {
                 for (int j = 0; j < players.size(); j++) {
-                    if (players.get(j).getMySchoolBoard().ColorTower().equals(colorWinning.get(i))){
-                        if(players.get(j).getMySchoolBoard().getProfessorTable().size()>max){
+                    if (players.get(j).getMySchoolBoard().ColorTower().equals(colorWinning.get(i))) {
+                        if (players.get(j).getMySchoolBoard().getProfessorTable().size() > max) {
                             colorTowers = new ArrayList<>();
                             colorTowers.add(colorWinning.get(i));
-                            max= players.get(j).getMySchoolBoard().getProfessorTable().size();
-                        }else if(players.get(j).getMySchoolBoard().getProfessorTable().size()==max){
+                            max = players.get(j).getMySchoolBoard().getProfessorTable().size();
+                        } else if (players.get(j).getMySchoolBoard().getProfessorTable().size() == max) {
                             colorTowers.add(colorWinning.get(i));
                         }
                     }
                 }
             }
-        }else{
+        } else {
             for (int j = 0; j < players.size(); j++) {
-                if(players.get(j).getMySchoolBoard().ColorTower().equals(ColorTower.Black)){
+                if (players.get(j).getMySchoolBoard().ColorTower().equals(ColorTower.Black)) {
                     ProfessorTableBlack.addAll(players.get(j).getMySchoolBoard().getProfessorTable());
                 }
-                if(players.get(j).getMySchoolBoard().ColorTower().equals(ColorTower.White)){
+                if (players.get(j).getMySchoolBoard().ColorTower().equals(ColorTower.White)) {
                     ProfessorTableWhite.addAll(players.get(j).getMySchoolBoard().getProfessorTable());
                 }
             }
 
-            if(ProfessorTableBlack.size()>ProfessorTableWhite.size()){
+            if (ProfessorTableBlack.size() > ProfessorTableWhite.size()) {
                 colorTowers.add(ColorTower.Black);
-            }else if(ProfessorTableBlack.size()<ProfessorTableWhite.size()){
+            } else if (ProfessorTableBlack.size() < ProfessorTableWhite.size()) {
                 colorTowers.add(ColorTower.White);
-            }else{
-                colorTowers= colorWinning;
+            } else {
+                colorTowers = colorWinning;
             }
         }
 
         return colorTowers;
     }
-
-    public boolean CheckIfWinWithProfessor(Player p, GeneralBoard g){
-        ArrayList<Integer> prof= new ArrayList<Integer>();
-        int k=1;
-        for(int i=0;i<g.getSchoolBoard().size();i++){
-            prof.add(g.getSchoolBoard().get(i).getProfessorTable().size());
-        }
-        for(int i=0;i<prof.size();i++){
-            for(int j=0;p.getMySchoolBoard().GetId()==i && j<prof.size();j++){
-                if(prof.get(i)>prof.get(j)){
-                    k++;
-                }
-            }
-        }
-        if(prof.size()==k){return true;}
-        else{return false;}
-    }
-    public boolean CheckIfWin(Player p, GeneralBoard g){
-        if(CheckIfWinWithTower(p,g) || CheckIfWinWithProfessor(p,g)){
-            return true;
-        }else{return false;}}
 }
+
--- src/test/java/it/polimi/ingsw/Controller/WinLoseCheckStateTest.java
+++ src/test/java/it/polimi/ingsw/Controller/WinLoseCheckStateTest.java
@@ -15,12 +15,15 @@ class WinLoseCheckStateTest {
     Player p1= null;
     Player p2= null;
     Player p3= null;
+    Player p4= null;
     SchoolBoard SB1 = null;
     SchoolBoard SB2 = null;
     SchoolBoard SB3 = null;
+    SchoolBoard SB4= null;
     DeckCardAssistant DCA1 = null;
     DeckCardAssistant DCA2 = null;
     DeckCardAssistant DCA3 = null;
+    DeckCardAssistant DCA4 = null;
 
     @BeforeEach
     public void setUp(){
@@ -29,12 +32,11 @@ class WinLoseCheckStateTest {
         SB1 = new SchoolBoard(0, 9);
         SB2 = new SchoolBoard(1, 9);
         SB3 = new SchoolBoard(2, 9);
+        SB4 = new SchoolBoard(3, 9);
         DCA1 = new DeckCardAssistant();
         DCA2 = new DeckCardAssistant();
         DCA3 = new DeckCardAssistant();
-        SB1.setColorTower(ColorTower.Black);
-        SB2.setColorTower(ColorTower.White);
-        SB3.setColorTower(ColorTower.Grey);
+        DCA4 = new DeckCardAssistant();
         p1=new Player("A");
         p1.setMySchoolBoard(SB1);
         p1.setNumberCoins(1);
@@ -47,10 +49,10 @@ class WinLoseCheckStateTest {
         p3.setMySchoolBoard(SB3);
         p3.setNumberCoins(1);
         p3.setMyDeck(DCA3);
-        SB1.setNumberOfTower(6);
-        SB2.setNumberOfTower(6);
-        SB3.setNumberOfTower(6);
-        g.CreateSchoolBoards(3);
+        p4= new Player("D");
+        p4.setMySchoolBoard(SB4);
+        p4.setNumberCoins(1);
+        p4.setMyDeck(DCA4);
     }
 
     @AfterEach
@@ -60,83 +62,25 @@ class WinLoseCheckStateTest {
         p1= null;
         p2= null;
         p3= null;
+        p4= null;
         SB1 = null;
         SB2 = null;
         SB3 = null;
+        SB4= null;
         DCA1 = null;
         DCA2 = null;
         DCA3 = null;
-    }
-    @Test
-    public void testCheckWinTowerBlack(){
-        g.CreateTwelveIslands();
-        g.GetIslands().get(0).putTower(ColorTower.Black);
-        g.GetIslands().get(2).putTower(ColorTower.Black);
-        g.GetIslands().get(3).putTower(ColorTower.Black);
-        g.GetIslands().get(6).putTower(ColorTower.White);
-        g.GetIslands().get(8).putTower(ColorTower.White);
-        g.GetIslands().get(10).putTower(ColorTower.Grey);
-        assertTrue(w.CheckIfWinWithTower(p1, g));
-        assertFalse(w.CheckIfWinWithTower(p2,g));
-        assertFalse(w.CheckIfWinWithTower(p3, g));
-        assertTrue(w.CheckIfWin(p1, g));
-        assertFalse(w.CheckIfWin(p2, g));
-        assertFalse(w.CheckIfWin(p3, g));
-    }
-    @Test
-    public void testCheckWinTowerWhite(){
-        g.CreateTwelveIslands();
-        g.GetIslands().get(0).putTower(ColorTower.White);
-        g.GetIslands().get(2).putTower(ColorTower.Black);
-        g.GetIslands().get(3).putTower(ColorTower.Black);
-        g.GetIslands().get(6).putTower(ColorTower.White);
-        g.GetIslands().get(8).putTower(ColorTower.White);
-        g.GetIslands().get(10).putTower(ColorTower.Grey);
-        assertTrue(w.CheckIfWinWithTower(p2, g));
-        assertFalse(w.CheckIfWinWithTower(p1,g));
-        assertFalse(w.CheckIfWinWithTower(p3, g));
-        assertTrue(w.CheckIfWin(p2, g));
-        assertFalse(w.CheckIfWin(p1, g));
-        assertFalse(w.CheckIfWin(p3, g));
-    }
-    @Test
-    public void testCheckWinTowerGrey(){
-        g.CreateTwelveIslands();
-        g.GetIslands().get(0).putTower(ColorTower.Grey);
-        g.GetIslands().get(2).putTower(ColorTower.Black);
-        g.GetIslands().get(3).putTower(ColorTower.Grey);
-        g.GetIslands().get(6).putTower(ColorTower.White);
-        g.GetIslands().get(8).putTower(ColorTower.White);
-        g.GetIslands().get(10).putTower(ColorTower.Grey);
-        assertTrue(w.CheckIfWinWithTower(p3, g));
-        assertFalse(w.CheckIfWinWithTower(p2,g));
-        assertFalse(w.CheckIfWinWithTower(p1, g));
-        assertTrue(w.CheckIfWin(p3, g));
-        assertFalse(w.CheckIfWin(p2, g));
-        assertFalse(w.CheckIfWin(p1, g));
-    }
-    @Test
-    public void testCheckWinProf(){
-      try {
-          g.getSchoolBoard().get(0).PutProfessor(Color.Red);
-          g.getSchoolBoard().get(0).PutProfessor(Color.Green);
-          g.getSchoolBoard().get(0).PutProfessor(Color.Pink);
-          g.getSchoolBoard().get(1).PutProfessor(Color.Blue);
-          g.getSchoolBoard().get(0).PutProfessor(Color.Yellow);
-      }catch (IllegalMoveException e){
-          fail();
-      }
-       
-       assertTrue(w.CheckIfWinWithProfessor(p1, g));
-       assertFalse(w.CheckIfWinWithProfessor(p2, g));
-       assertFalse(w.CheckIfWinWithProfessor(p3, g));
-       assertTrue(w.CheckIfWin(p1, g));
-       assertFalse(w.CheckIfWin(p2, g));
-       assertFalse(w.CheckIfWin(p3, g));
+        DCA4 = null;
     }
 
     @Test
-    public void testCheckWinner(){
+    public void testCheckWinnerWithoutCheckingProfessor(){
+      SB1.setColorTower(ColorTower.Black);
+      SB2.setColorTower(ColorTower.White);
+      SB3.setColorTower(ColorTower.Grey);
+      SB1.setNumberOfTower(6);
+      SB2.setNumberOfTower(6);
+      SB3.setNumberOfTower(6);
       p1.getMySchoolBoard().RemoveTower();
       p2.getMySchoolBoard().RemoveTower();
       p2.getMySchoolBoard().RemoveTower();
@@ -157,7 +101,47 @@ class WinLoseCheckStateTest {
 
 
     @Test
-    public void testCheckWinner2(){
+    public void testCheckWinnerCheckingProfessor4players(){
+        SB1.setColorTower(ColorTower.Black);
+        SB2.setColorTower(ColorTower.White);
+        SB3.setColorTower(ColorTower.Black);
+        SB4.setColorTower(ColorTower.White);
+        SB1.setNumberOfTower(8);
+        SB2.setNumberOfTower(8);
+        p1.getMySchoolBoard().RemoveTower();
+        p2.getMySchoolBoard().RemoveTower();
+        ArrayList<SchoolBoard> schoolBoards= new ArrayList<>();
+        ArrayList<Player> players = new ArrayList<>();
+        ArrayList<Boolean> winners;
+        schoolBoards.add(0,SB1);
+        schoolBoards.add(1,SB2);
+        players.add(0,p1);
+        players.add(1,p2);
+        players.add(2,p3);
+        players.add(3,p4);
+        try {
+            players.get(2).getMySchoolBoard().PutProfessor(Color.Red);
+            players.get(1).getMySchoolBoard().PutProfessor(Color.Green);
+            players.get(0).getMySchoolBoard().PutProfessor(Color.Pink);
+            players.get(3).getMySchoolBoard().PutProfessor(Color.Blue);
+        }catch (IllegalMoveException e){
+            fail();
+        }
+        winners= w.CheckWinner(players,schoolBoards);
+        assertTrue(winners.get(0));
+        assertTrue(winners.get(1));
+        assertTrue(winners.get(2));
+        assertTrue(winners.get(3));
+    }
+
+    @Test
+    public void testCheckWinnerCheckingProfessor3players(){
+        SB1.setColorTower(ColorTower.Black);
+        SB2.setColorTower(ColorTower.White);
+        SB3.setColorTower(ColorTower.Grey);
+        SB1.setNumberOfTower(6);
+        SB2.setNumberOfTower(6);
+        SB3.setNumberOfTower(6);
         p1.getMySchoolBoard().RemoveTower();
         p2.getMySchoolBoard().RemoveTower();
         p2.getMySchoolBoard().RemoveTower();
@@ -173,11 +157,11 @@ class WinLoseCheckStateTest {
         players.add(1,p2);
         players.add(2,p3);
         try {
-            g.getSchoolBoard().get(2).PutProfessor(Color.Red);
-            g.getSchoolBoard().get(2).PutProfessor(Color.Green);
-            g.getSchoolBoard().get(0).PutProfessor(Color.Pink);
-            g.getSchoolBoard().get(0).PutProfessor(Color.Blue);
-            g.getSchoolBoard().get(0).PutProfessor(Color.Yellow);
+            schoolBoards.get(2).PutProfessor(Color.Red);
+            schoolBoards.get(2).PutProfessor(Color.Green);
+            schoolBoards.get(0).PutProfessor(Color.Pink);
+            schoolBoards.get(0).PutProfessor(Color.Blue);
+            schoolBoards.get(0).PutProfessor(Color.Yellow);
         }catch (IllegalMoveException e){
             fail();
         }
@@ -186,6 +170,70 @@ class WinLoseCheckStateTest {
         assertFalse(winners.get(0));
         assertFalse(winners.get(1));
         assertTrue(winners.get(2));
+    }
+
+    @Test
+    public void testCheckWinnerCheckingProfessor2players(){
+        SB1.setColorTower(ColorTower.Black);
+        SB2.setColorTower(ColorTower.White);
+        SB1.setNumberOfTower(8);
+        SB2.setNumberOfTower(8);
+        p1.getMySchoolBoard().RemoveTower();
+        p2.getMySchoolBoard().RemoveTower();
+        ArrayList<SchoolBoard> schoolBoards= new ArrayList<>();
+        ArrayList<Player> players = new ArrayList<>();
+        ArrayList<Boolean> winners;
+        schoolBoards.add(0,SB1);
+        schoolBoards.add(1,SB2);
+        players.add(0,p1);
+        players.add(1,p2);
+        try {
+            schoolBoards.get(1).PutProfessor(Color.Red);
+            schoolBoards.get(1).PutProfessor(Color.Green);
+            schoolBoards.get(0).PutProfessor(Color.Pink);
+            schoolBoards.get(0).PutProfessor(Color.Blue);
+            schoolBoards.get(0).PutProfessor(Color.Yellow);
+        }catch (IllegalMoveException e){
+            fail();
+        }
+        winners= w.CheckWinner(players,schoolBoards);
+
+        assertTrue(winners.get(0));
+        assertFalse(winners.get(1));
+    }
+    @Test
+    public void testCheckWinnerCheckingProfessor4players2(){
+        SB1.setColorTower(ColorTower.Black);
+        SB2.setColorTower(ColorTower.White);
+        SB3.setColorTower(ColorTower.Black);
+        SB4.setColorTower(ColorTower.White);
+        SB1.setNumberOfTower(8);
+        SB2.setNumberOfTower(8);
+        p1.getMySchoolBoard().RemoveTower();
+        p2.getMySchoolBoard().RemoveTower();
+        ArrayList<SchoolBoard> schoolBoards= new ArrayList<>();
+        ArrayList<Player> players = new ArrayList<>();
+        ArrayList<Boolean> winners;
+        schoolBoards.add(0,SB1);
+        schoolBoards.add(1,SB2);
+        players.add(0,p1);
+        players.add(1,p2);
+        players.add(2,p3);
+        players.add(3,p4);
+        try {
+            players.get(2).getMySchoolBoard().PutProfessor(Color.Red);
+            players.get(1).getMySchoolBoard().PutProfessor(Color.Green);
+            players.get(0).getMySchoolBoard().PutProfessor(Color.Pink);
+            players.get(3).getMySchoolBoard().PutProfessor(Color.Blue);
+            players.get(3).getMySchoolBoard().PutProfessor(Color.Yellow);
+        }catch (IllegalMoveException e){
+            fail();
+        }
+        winners= w.CheckWinner(players,schoolBoards);
+        assertFalse(winners.get(0));
+        assertTrue(winners.get(1));
+        assertFalse(winners.get(2));
+        assertTrue(winners.get(3));
     }
 
 }
