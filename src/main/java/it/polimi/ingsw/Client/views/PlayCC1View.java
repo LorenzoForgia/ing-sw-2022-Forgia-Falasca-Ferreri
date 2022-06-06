2b3f50afdada1476bc9cbed09d5a53316919385e
federicoalferreri <federicoangelo.ferreri@mail.polimi.it>
2022-06-06 23:46:44 +0200

    View CLI

--- src/main/java/it/polimi/ingsw/Client/views/PlayCC1View.java
+++ src/main/java/it/polimi/ingsw/Client/views/PlayCC1View.java
@@ -31,44 +31,52 @@ public class PlayCC1View extends View{
         displayer.displayAllcharactercard(answerMsg.GetCharacterCards());
         System.out.println("Vuoi giocare una carta personaggio? si/no");
         String card= scanner.nextLine();
-        if(card.equals("si")){
-            System.out.println("Qual'è il numero della carta personaggio che vuoi giocare?");
-            int numcard=Integer.parseInt(scanner.nextLine());
-            CCMsg ccMsg= new CCMsg(numcard);
-            getOwner().getServerHandler().sendCommandMessage(ccMsg);
-        }else{
-            Boolean flag=false;
-            int count=0;
-            int isl;
-            Color cdef=Color.Blue;
-            while(!flag) {
-                if(count==0) {
-                    System.out.println("Scegli il colore del primo studente che vuoi spostare");
-                }else{
-                    System.out.println("Errore inserimento colore:Seleziona un colore valido");
-                }
-                String colorchosen = scanner.nextLine();
-                Color[] colors = Color.values();
-                for (int i = 0; i < 5 && !flag; i++) {
-                    if (colors[i].getName().equals(colorchosen)) {
-                        cdef=colors[i];
-                        flag = true;
+        boolean f=false;
+        while(!f) {
+            if(card.equals("si")){
+                System.out.println("Qual'è il numero della carta personaggio che vuoi giocare?");
+                f=true;
+                int numcard=Integer.parseInt(scanner.nextLine());
+                CCMsg ccMsg= new CCMsg(numcard);
+                getOwner().getServerHandler().sendCommandMessage(ccMsg);
+            }else if(card.equals("no")){
+                f=true;
+                Boolean flag=false;
+                int count=0;
+                int isl;
+                Color cdef=Color.Blue;
+                while(!flag) {
+                    if(count==0) {
+                        System.out.println("Scegli il colore del primo studente che vuoi spostare");
+                    }else{
+                        System.out.println("Errore inserimento colore:Seleziona un colore valido");
                     }
+                    String colorchosen = scanner.nextLine();
+                    Color[] colors = Color.values();
+                    for (int i = 0; i < 5 && !flag; i++) {
+                        if (colors[i].getName().equals(colorchosen)) {
+                            cdef=colors[i];
+                            flag = true;
+                        }
+                    }
+                    count++;
                 }
-                count++;
-            }
-            Color student= cdef;
-            System.out.println("Vuoi spostarlo nella sala o su un'isola? sala/isola");
-            String ris= scanner.nextLine();
-            if(ris.equals("sala")){
-                isl=12;
-                MoveStudent1Msg moveStudentMsg= new MoveStudent1Msg(student, isl);
-                getOwner().getServerHandler().sendCommandMessage(moveStudentMsg);
+                Color student= cdef;
+                System.out.println("Vuoi spostarlo nella sala o su un'isola? sala/isola");
+                String ris= scanner.nextLine();
+                if(ris.equals("sala")){
+                    isl=12;
+                    MoveStudent1Msg moveStudentMsg= new MoveStudent1Msg(student, isl);
+                    getOwner().getServerHandler().sendCommandMessage(moveStudentMsg);
+                }else{
+                    System.out.println("Su quale isola vuoi spostarlo?");
+                    isl=Integer.parseInt(scanner.nextLine());
+                    MoveStudent1Msg moveStudentMsg= new MoveStudent1Msg(student, isl);
+                    getOwner().getServerHandler().sendCommandMessage(moveStudentMsg);
+                }
             }else{
-                System.out.println("Su quale isola vuoi spostarlo?");
-                isl=Integer.parseInt(scanner.nextLine());
-                MoveStudent1Msg moveStudentMsg= new MoveStudent1Msg(student, isl);
-                getOwner().getServerHandler().sendCommandMessage(moveStudentMsg);
+                System.out.println("Errore inserimento! Riprova");
+                card= scanner.nextLine();
             }
         }
     }
