67b332326d68ece836b082aac7317b2714b1a09a
LorenzoForgia <lorenzo.forgia@mail.polimi.it>
2022-06-06 23:29:16 +0200

    added some features to CLI

--- src/main/java/it/polimi/ingsw/Client/views/MoveStudent4AfterNotCCView.java
+++ src/main/java/it/polimi/ingsw/Client/views/MoveStudent4AfterNotCCView.java
@@ -21,16 +21,8 @@ public class MoveStudent4AfterNotCCView extends View{
         Scanner scanner = new Scanner(System.in);
         Displayer displayer = new Displayer();
         int isl=0;
-        ArrayList<SchoolBoard> schoolBoards = new ArrayList<SchoolBoard>();
-        for (int i = 0; i < answerMsg.GetGB().getSchoolBoard().size(); i++) {
-            schoolBoards.add(answerMsg.GetGB().getSchoolBoard().get(i));
-        }
-        displayer.displayAllSchoolboard(schoolBoards, answerMsg.GetPlayers());
-        ArrayList<IslandTiles> islandTiles = new ArrayList<>();
-        for (int i = 0; i < answerMsg.GetGB().GetIslands().size(); i++) {
-            islandTiles.add(answerMsg.GetGB().GetIslands().get(i));
-        }
-        displayer.showAllIsland(islandTiles);
+        displayer.displayAllSchoolboard(answerMsg.GetGB().getSchoolBoard(),answerMsg.GetPlayers());
+        displayer.showAllIsland(answerMsg.GetGB().GetIslands());
         Boolean flag = false;
         int count = 0;
         Color cdef = Color.Blue;
