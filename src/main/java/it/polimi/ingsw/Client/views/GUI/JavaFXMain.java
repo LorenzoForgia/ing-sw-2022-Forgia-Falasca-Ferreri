1778dfa34c41802985b089962711b69a338d0fbf
LuigiaFalasca <luigia.falasca@mail.polimi.it>
2022-06-10 22:15:19 +0200

    GUI

--- src/main/java/it/polimi/ingsw/Client/views/GUI/JavaFXMain.java
+++ src/main/java/it/polimi/ingsw/Client/views/GUI/JavaFXMain.java
@@ -1,7 +1,16 @@
 package it.polimi.ingsw.Client.views.GUI;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class JavaFXMain extends Application {
 
@@ -9,8 +18,32 @@ public class JavaFXMain extends Application {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    private Stage primaryStage;
 
+
+    @Override
+    public void start(Stage primaryStage) throws Exception
+    {
+        Group root = new Group();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+
+    public void switchToLoginScene()
+    {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/LoginScene.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        Scene sc = new Scene(root);
+        primaryStage.setScene(sc);
+        primaryStage.setTitle("Login");
+        primaryStage.sizeToScene();
     }
 }
