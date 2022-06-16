fe9d59bdab01983fea1d2efff0c45510440af522
LuigiaFalasca <luigia.falasca@mail.polimi.it>
2022-06-16 15:20:41 +0200

    GUI

--- src/main/java/it/polimi/ingsw/Client/views/GUI/IslandTilesScene.java
+++ src/main/java/it/polimi/ingsw/Client/views/GUI/IslandTilesScene.java
@@ -1,21 +1,202 @@
 package it.polimi.ingsw.Client.views.GUI;
 
+import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class IslandTilesScene {
-    public ImageView island1;
-    public ImageView island2;
-    public ImageView island3;
-    public ImageView island4;
-    public ImageView island5;
-    public ImageView island6;
-    public ImageView island7;
-    public ImageView island8;
-    public ImageView island9;
-    public ImageView island10;
-    public ImageView island11;
-    public ImageView island12;
+
+    @FXML
+    private ImageView island1;
+    @FXML
+    private ImageView island2;
+    @FXML
+    private ImageView island3;
+    @FXML
+    private ImageView island4;
+    @FXML
+    private ImageView island5;
+    @FXML
+    private ImageView island6;
+    @FXML
+    private ImageView island7;
+    @FXML
+    private ImageView island8;
+    @FXML
+    private ImageView island9;
+    @FXML
+    private ImageView island11;
+    @FXML
+    private ImageView island12;
+    @FXML
+    private ImageView island10;
+    @FXML
+    private ImageView studentRed1;
+    @FXML
+    private ImageView studentRed2;
+    @FXML
+    private ImageView studentRed3;
+    @FXML
+    private ImageView studentRed4;
+    @FXML
+    private ImageView studentRed5;
+    @FXML
+    private ImageView studentRed6;
+    @FXML
+    private ImageView studentRed7;
+    @FXML
+    private ImageView studentRed8;
+    @FXML
+    private ImageView studentRed9;
+    @FXML
+    private ImageView studentRed10;
+    @FXML
+    private ImageView studentRed11;
+    @FXML
+    private ImageView studentRed12;
+    @FXML
+    private ImageView studentYellow1;
+    @FXML
+    private ImageView studentYellow2;
+    @FXML
+    private ImageView studentYellow3;
+    @FXML
+    private ImageView studentYellow4;
+    @FXML
+    private ImageView studentYellow5;
+    @FXML
+    private ImageView studentYellow6;
+    @FXML
+    private ImageView studentYellow7;
+    @FXML
+    private ImageView studentYellow8;
+    @FXML
+    private ImageView studentYellow9;
+    @FXML
+    private ImageView studentYellow10;
+    @FXML
+    private ImageView studentYellow11;
+    @FXML
+    private ImageView studentYellow12;
+    @FXML
+    private ImageView studentGreen1;
+    @FXML
+    private ImageView studentGreen2;
+    @FXML
+    private ImageView studentGreen3;
+    @FXML
+    private ImageView studentGreen4;
+    @FXML
+    private ImageView studentGreen5;
+    @FXML
+    private ImageView studentGreen6;
+    @FXML
+    private ImageView studentGreen7;
+    @FXML
+    private ImageView studentGreen8;
+    @FXML
+    private ImageView studentGreen9;
+    @FXML
+    private ImageView studentGreen10;
+    @FXML
+    private ImageView studentGreen11;
+    @FXML
+    private ImageView studentGreen12;
+    @FXML
+    private ImageView studentPink1;
+    @FXML
+    private ImageView studentPink2;
+    @FXML
+    private ImageView studentPink3;
+    @FXML
+    private ImageView studentPink4;
+    @FXML
+    private ImageView studentPink5;
+    @FXML
+    private ImageView studentPink6;
+    @FXML
+    private ImageView studentPink7;
+    @FXML
+    private ImageView studentPink8;
+    @FXML
+    private ImageView studentPink9;
+    @FXML
+    private ImageView studentPink10;
+    @FXML
+    private ImageView studentPink11;
+    @FXML
+    private ImageView studentPink12;
+    @FXML
+    private ImageView studentBlue1;
+    @FXML
+    private ImageView studentBlue2;
+    @FXML
+    private ImageView studentBlue3;
+    @FXML
+    private ImageView studentBlue4;
+    @FXML
+    private ImageView studentBlue5;
+    @FXML
+    private ImageView studentBlue6;
+    @FXML
+    private ImageView studentBlue7;
+    @FXML
+    private ImageView studentBlue8;
+    @FXML
+    private ImageView studentBlue9;
+    @FXML
+    private ImageView studentBlue10;
+    @FXML
+    private ImageView studentBlue11;
+    @FXML
+    private ImageView studentBlue12;
+
+    public void chosenIsland1(){
+        System.out.println("Ho scelto l'isola 1");
+    }
+    public void chosenIsland2(){
+        System.out.println("Ho scelto l'isola 2");
+    }
+    public void chosenIsland3(){
+        System.out.println("Ho scelto l'isola 3");
+    }
+    public void chosenIsland4(){
+        System.out.println("Ho scelto l'isola 4");
+    }
+    public void chosenIsland5(){
+        System.out.println("Ho scelto l'isola 5");
+    }
+    public void chosenIsland6(){
+        System.out.println("Ho scelto l'isola 6");
+    }
+    public void chosenIsland7(){
+        System.out.println("Ho scelto l'isola 7");
+    }
+    public void chosenIsland8(){
+        System.out.println("Ho scelto l'isola 8");
+    }
+    public void chosenIsland9(){
+        System.out.println("Ho scelto l'isola 9");
+    }
+    public void chosenIsland10(){
+        System.out.println("Ho scelto l'isola 10");
+    }
+    public void chosenIsland11(){
+        System.out.println("Ho scelto l'isola 11");
+    }
+    public void chosenIsland12(){
+        System.out.println("Ho scelto l'isola 12");
+    }
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ src/main/java/it/polimi/ingsw/Client/views/GUI/TurnDecideScene.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw.Client.views.GUI;
+
+import it.polimi.ingsw.messages.FirstPlayerTurnMsg;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class TurnDecideScene {
+    @FXML
+    private Label player1;
+    @FXML
+    private Label player2;
+    @FXML
+    private Label player3;
+    @FXML
+    private Label player4;
+    public void initialize(){
+        /*
+        for(int i=0; i<answerMsg.getNickname().size(); i++){
+            if(i==0){
+                player1.setText(answerMsg.getNickname().get(i));
+            }else if(i==1){
+                player2.setText(answerMsg.getNickname().get(i));
+            }else if(i==2){
+                player3.setText(answerMsg.getNickname().get(i));
+            }else{
+                player4.setText(answerMsg.getNickname().get(i));
+            }
+        }
+        FirstPlayerTurnMsg firstPlayerTurnMsg = new FirstPlayerTurnMsg();
+        JavaFXMain.getCurrentApplication().getClient().getServerHandler().sendCommandMessage(firstPlayerTurnMsg);*/
+    }
+}
