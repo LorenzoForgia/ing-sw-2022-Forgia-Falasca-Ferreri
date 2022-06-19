cc3ee01046fd1de473f561081ef732a7497c3d06
LuigiaFalasca <luigia.falasca@mail.polimi.it>
2022-06-19 21:56:05 +0200

    GUI

--- src/main/java/it/polimi/ingsw/Client/views/GUI/MoveStudent1Scene.java
+++ src/main/java/it/polimi/ingsw/Client/views/GUI/MoveStudent1Scene.java
@@ -1,14 +1,54 @@
 package it.polimi.ingsw.Client.views.GUI;
 
 import it.polimi.ingsw.Model.Color;
+import it.polimi.ingsw.messages.AnsAskCAMsg;
+import it.polimi.ingsw.messages.AnsFirstPlayerTurnMsg;
 import it.polimi.ingsw.messages.MoveStudent1Msg;
 import javafx.event.ActionEvent;
-import java.awt.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.*;
 
 
 public class MoveStudent1Scene {
     private static Color color;
     private static int location;
+    @FXML
+    private ImageView imageIsland;
+    @FXML
+    private ImageView imageFirstSchoolBoard;
+    @FXML
+    private ImageView imageSecondSchoolBoard;
+    @FXML
+    private ImageView imageThirdSchoolBoard;
+    @FXML
+    private ImageView imageFourthSchoolBoard;
+    @FXML
+    private Label showSchoolBoard1;
+    @FXML
+    private Label showSchoolBoard2;
+    @FXML
+    private Label showSchoolBoard3;
+    @FXML
+    private Label showSchoolBoard4;
+
+    private static AnsAskCAMsg answerMsg;
+
+    public static void setAnswerMsg(AnsAskCAMsg answerMsg) {
+        MoveStudent1Scene.answerMsg = answerMsg;
+    }
+
+    public void initialize() {
+        if(answerMsg.GetPlayers().size()==2){
+            imageFourthSchoolBoard.setImage(null);
+            imageThirdSchoolBoard.setImage(null);
+        }else if(answerMsg.GetPlayers().size()==3){
+
+        }else{
+
+        }
+    }
+
 
     public void BlueSelected(){
         color=Color.Blue;
@@ -39,10 +79,23 @@ public class MoveStudent1Scene {
         JavaFXMain.getCurrentApplication().switchToIslandTitleScene();
     }
 
+    public void showGeneralBoardSelected(){
 
+    }
 
+    public void showShowSchoolBoard1Selected(){
 
+    }
+    public void showShowSchoolBoard2Selected(){
 
+    }
+    public void showShowSchoolBoard3Selected(){
+
+    }
+
+    public void showShowSchoolBoard4Selected(){
+
+    }
 
 
 }
