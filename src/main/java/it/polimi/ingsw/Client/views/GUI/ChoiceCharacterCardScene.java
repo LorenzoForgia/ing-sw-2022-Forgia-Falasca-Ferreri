5978ecc7f36e1978461e2844bbbea9996c160797
federicoalferreri <federicoangelo.ferreri@mail.polimi.it>
2022-06-22 10:01:45 +0200

    GUI

--- src/main/java/it/polimi/ingsw/Client/views/GUI/ChoiceCharacterCardScene.java
+++ src/main/java/it/polimi/ingsw/Client/views/GUI/ChoiceCharacterCardScene.java
@@ -3,7 +3,6 @@ package it.polimi.ingsw.Client.views.GUI;
 import it.polimi.ingsw.Model.CharacterCard;
 import it.polimi.ingsw.messages.AnsAskCAMsg;
 import it.polimi.ingsw.messages.CCMsg;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
@@ -42,27 +41,27 @@ public class ChoiceCharacterCardScene {
             if (cards.get(i).getName() == 1) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front.jpg"));
             } else if (cards.get(i).getName() == 2) {
-                imageCharacterCard.get(i).setImage(new Image("CarteTOT_front2.jpg"));
-            } else if (cards.get(i).getName() == 3) {
-                imageCharacterCard.get(i).setImage(new Image("CarteTOT_front3.jpg"));
-            } else if (cards.get(i).getName() == 4) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front4.jpg"));
-            } else if (cards.get(i).getName() == 5) {
+            } else if (cards.get(i).getName() == 3) {
+                imageCharacterCard.get(i).setImage(new Image("CarteTOT_front4.jpg"));
+            } else if (cards.get(i).getName() == 4) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front5.jpg"));
-            } else if (cards.get(i).getName() == 6) {
+            } else if (cards.get(i).getName() == 5) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front6.jpg"));
-            } else if (cards.get(i).getName() == 7) {
+            } else if (cards.get(i).getName() == 6) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front7.jpg"));
-            } else if (cards.get(i).getName() == 8) {
+            } else if (cards.get(i).getName() == 7) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front8.jpg"));
-            } else if (cards.get(i).getName() == 9) {
+            } else if (cards.get(i).getName() == 8) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front9.jpg"));
-            } else if (cards.get(i).getName() == 10) {
+            } else if (cards.get(i).getName() == 9) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front10.jpg"));
-            } else if (cards.get(i).getName() == 11) {
+            } else if (cards.get(i).getName() == 10) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front11.jpg"));
-            } else if (cards.get(i).getName() == 12) {
+            } else if (cards.get(i).getName() == 11) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front12.jpg"));
+            } else if (cards.get(i).getName() == 12) {
+                imageCharacterCard.get(i).setImage(new Image("CarteTOT_front2.jpg"));
             }
         }
     }
--- src/main/java/it/polimi/ingsw/Client/views/GUI/PlayCC1Scene.java
+++ src/main/java/it/polimi/ingsw/Client/views/GUI/PlayCC1Scene.java
@@ -52,27 +52,27 @@ public class PlayCC1Scene {
             if (answerMsg.GetCharacterCards().get(i).getName() == 1) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front.jpg"));
             } else if (answerMsg.GetCharacterCards().get(i).getName() == 2) {
-                imageCharacterCard.get(i).setImage(new Image("CarteTOT_front2.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 3) {
-                imageCharacterCard.get(i).setImage(new Image("CarteTOT_front3.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 4) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front4.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 5) {
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 3) {
+                imageCharacterCard.get(i).setImage(new Image("CarteTOT_front4.jpg"));
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 4) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front5.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 6) {
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 5) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front6.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 7) {
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 6) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front7.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 8) {
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 7) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front8.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 9) {
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 8) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front9.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 10) {
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 9) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front10.jpg"));
-            }else if (answerMsg.GetCharacterCards().get(i).getName() == 11) {
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 10) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front11.jpg"));
-            } else if (answerMsg.GetCharacterCards().get(i).getName() == 12) {
+            }else if (answerMsg.GetCharacterCards().get(i).getName() == 11) {
                 imageCharacterCard.get(i).setImage(new Image("CarteTOT_front12.jpg"));
+            } else if (answerMsg.GetCharacterCards().get(i).getName() == 12) {
+                imageCharacterCard.get(i).setImage(new Image("CarteTOT_front2.jpg"));
             }
 
         }
